80acf54065d4273c6f8097aca8631b69d31b18b6
Giselle Souza <reallovetour@gmail.com>
2023-04-20 12:06:41 -0300

    Controller produto e categoria

--- src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
+++ src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
@@ -4,14 +4,17 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.farmacia.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
-	public List <Produto> findByNomeOrFornecedor(String nome, String fornecedor);
+	public List <Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
-	public List <Produto> findByNomeAndFornecedor(String nome, String fornecedor);
+	public List <Produto> findByNomeOrLaboratorio(String nome, String laboratorio);
+
+	public List <Produto> findByNomeAndLaboratorio(String nome, String laboratorio);
 
 	public List <Produto> findByPrecoBetween(BigDecimal inicio, BigDecimal fim);
 
